9b674ea31d1c3196de1081019e8a784e868651d2
Ege Yıldırım <85133392+egeyil@users.noreply.github.com>
2022-05-03 20:54:04 +0300

    add about me section

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const openBtn = document.getElementById("about-me");
+const closeBtn = document.getElementById("close");
+const modal = document.getElementById("bio");
+
+function openModal() {
+  modal.classList.remove("invisible");
+  modal.classList.add("open-modal");
+  setTimeout(() => {
+    modal.classList.remove("open-modal");
+  }, 1000);
+}
+
+function closeModal() {
+  modal.classList.add("close-modal");
+  setTimeout(() => {
+    modal.classList.add("invisible");
+    modal.classList.remove("close-modal");
+  }, 1000);
+}
+
+openBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+// modal.addEventListener("click", function (e) {
+//   if (e.target.classList.contains("bio")) {
+//     console.log("zart");
+//     closeModal();
+//   }
+// });
